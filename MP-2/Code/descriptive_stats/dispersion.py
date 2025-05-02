f2d4cd845203df35fd2ab7c1297a329d5849c14c
Nikolaj1992 <cph-nf70@cphbusiness.dk>
2025-05-02 09:50:16 +0200

    Task11-answered
    Task 11 should be done. Also fixed task 7 and one of our modules.

--- MP-2/Code/descriptive_stats/dispersion.py
+++ MP-2/Code/descriptive_stats/dispersion.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+__all__ = ['variance', 'standard_deviation', 'range_stat', 'iqr', 'iqr_outliers']
+
 def variance(data):
     return np.var(data, ddof=1)
 
@@ -12,3 +14,11 @@ def range_stat(data):
 def iqr(data):
     q3, q1 = np.percentile(data, [75, 25])
     return q3 - q1
+
+def iqr_outliers(data, column):
+    q1 = data[column].quantile(0.25)
+    q3 = data[column].quantile(0.75) 
+    iqr = q3 - q1
+    lower_bound = q1 - 1.5 * iqr
+    upper_bound = q3 + 1.5 * iqr
+    return data[(data[column] < lower_bound) | (data[column] > upper_bound)]
